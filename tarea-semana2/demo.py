f81710c280df1cc727d663f128b4316fcb0db7d9
Sergio Blanco <sergio.blanco@ucr.ac.cr>
2025-07-23 20:48:12 -0600

    Documento Final

--- tarea-semana2/demo.py
+++ tarea-semana2/demo.py
@@ -1,6 +1,8 @@
-import numpy as np
-import pandas as pd
 
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import linalg as LA
+import math
 
 vector1 = [1,2,3]
 vector2 = [10,20,30]
@@ -28,7 +30,7 @@ columns1 = len(vector1)
 columns2 = len(vector2)
 factor = vector1[0] / vector2[0]
 
-
+"""
 for i in range(0,columns1):
     vector3.append(vector2[i] * factor)
     
@@ -36,3 +38,37 @@ if vector1 == vector3 :
     print("colineales")
 else:
     print("no colineales")    
+    
+
+"""
+
+vector1 = [1,2]
+vector2 = [10,20]
+
+vector1 = np.array([1.,2.] )
+vector2 = np.array([10.,20.] )
+
+fig, ax = plt.subplots()
+ax.quiver(vector1, vector2,color=["r","b"],angles='xy', scale_units='xy', scale=1)
+ax.axis([-3.,20.,-3.,23.])
+ax.set_aspect('equal')
+plt.show()
+
+
+vector3 = []
+columns1 = len(vector1)
+columns2 = len(vector2)
+factor = vector1[0] / vector2[0]
+
+
+for i in range(0,columns1):
+    vector3.append(vector2[i] * factor)
+
+vector3 = np.array(vector3 )
+#vector3 = np.array( vector3 )
+if np.array_equal(vector1 ,vector3): 
+    display(Markdown("## Respuesta :"))
+    print("--->   Son colineales")
+else:
+    display(Markdown("## Respuesta :"))
+    print("--->   No son colineales")
